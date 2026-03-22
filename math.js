41f13f9150f8787caf2a2a29e514e5808012dc81
Gautam Sah <gautamsah@example.com>
2026-03-22 13:38:26 +0545

    mandaatory commit mersesage

---
+++ math.js
@@ -0,0 +1,22 @@
+// const sum= (a,b)=>a+b;
+// const mul = (a,b)=>a*b;
+// const g = 9.8;
+// const pi = 3.14;
+
+
+// module.exports= 123;
+
+// module.exports = {
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     PI:pi,
+// };
+
+// module.exports.sum=(a,b)=> a +b;
+// exports.name="gautam"
+
+export const sum= (a,b)=>a+b;
+export const mul = (a,b)=>a*b;
+export const g = 9.8;
+export const pi = 3.14;
